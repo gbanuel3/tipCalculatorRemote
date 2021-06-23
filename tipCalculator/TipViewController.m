a9ac1a018ac2623e0eb394df454d2dec67406dd3
Gildardo Banuelos <gbanuel@fb.com>
2021-06-22 20:36:57 -0500

    Tip Calculator Required Components

---
+++ tipCalculator/TipViewController.m
@@ -0,0 +1,47 @@
+//
+//  TipViewController.m
+//  tipCalculator
+//
+//  Created by Gildardo Banuelos on 6/22/21.
+//
+
+#import "TipViewController.h"
+
+@interface TipViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *billField;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (weak, nonatomic) IBOutlet UITextField *tipPercentageField;
+
+@end
+
+@implementation TipViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+- (IBAction)onTap:(id)sender {
+    [self.view endEditing:true];
+}
+- (IBAction)updateLabels:(id)sender {
+    double tipPercentage = [self.tipPercentageField.text doubleValue];
+    double bill = [self.billField.text doubleValue];
+    double tip = bill * tipPercentage/100;
+    double total = bill + tip;
+    
+    self.tipLabel.text = [NSString stringWithFormat:@"$%.2f", tip];
+    self.totalLabel.text = [NSString stringWithFormat:@"$%.2f", total];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
